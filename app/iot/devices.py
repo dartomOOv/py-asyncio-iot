43b3941f1b64c920838b338fbf3947381d27f0ba
Danyil <dartomov@gmail.com>
2024-10-24 16:14:16 +0200

    Solution

--- app/iot/devices.py
+++ app/iot/devices.py
@@ -1,13 +1,11 @@
 import asyncio
 
 from .message import MessageType
-from .service import Device
 
 TIME_TO_SLEEP = 0.5
 
 
-# of course this code looks dumb, but imagine some real implementations of each method here
-class HueLightDevice(Device):
+class HueLightDevice:
     async def connect(self) -> None:
         print("Connecting Hue Light.")
         await asyncio.sleep(TIME_TO_SLEEP)
@@ -18,15 +16,21 @@ class HueLightDevice(Device):
         await asyncio.sleep(TIME_TO_SLEEP)
         print("Hue Light disconnected.")
 
-    async def send_message(self, message_type: MessageType, data: str = "") -> None:
+    async def send_message(
+            self,
+            message_type: MessageType,
+            data: str = ""
+    ) -> None:
+
         print(
-            f"Hue Light handling message of type {message_type.name} with data [{data}]."
+            f"Hue Light handling "
+            f"message of type {message_type.name} with data [{data}]."
         )
         await asyncio.sleep(TIME_TO_SLEEP)
         print("Hue Light received message.")
 
 
-class SmartSpeakerDevice(Device):
+class SmartSpeakerDevice:
     async def connect(self) -> None:
         print("Connecting to Smart Speaker.")
         await asyncio.sleep(TIME_TO_SLEEP)
@@ -37,15 +41,20 @@ class SmartSpeakerDevice(Device):
         await asyncio.sleep(TIME_TO_SLEEP)
         print("Smart Speaker disconnected.")
 
-    async def send_message(self, message_type: MessageType, data: str = "") -> None:
+    async def send_message(
+            self,
+            message_type: MessageType,
+            data: str = ""
+    ) -> None:
         print(
-            f"Smart Speaker handling message of type {message_type.name} with data [{data}]."
+            f"Smart Speaker handling "
+            f"message of type {message_type.name} with data [{data}]."
         )
         await asyncio.sleep(TIME_TO_SLEEP)
         print("Smart Speaker received message.")
 
 
-class SmartToiletDevice(Device):
+class SmartToiletDevice:
     async def connect(self) -> None:
         print("Connecting to Smart Toilet.")
         await asyncio.sleep(TIME_TO_SLEEP)
@@ -56,9 +65,15 @@ class SmartToiletDevice(Device):
         await asyncio.sleep(TIME_TO_SLEEP)
         print("Smart Toilet disconnected.")
 
-    async def send_message(self, message_type: MessageType, data: str = "") -> None:
+    async def send_message(
+            self,
+            message_type: MessageType,
+            data: str = ""
+    ) -> None:
+
         print(
-            f"Smart Toilet handling message of type {message_type.name} with data [{data}]."
+            f"Smart Toilet handling "
+            f"message of type {message_type.name} with data [{data}]."
         )
         await asyncio.sleep(TIME_TO_SLEEP)
         print("Smart Toilet received message.")
--- app/main.py
+++ app/main.py
@@ -1,64 +1,49 @@
 import asyncio
 import time
-from typing import Awaitable, Any
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-async def run_sequence(*functions: Awaitable[Any]) -> None:
-    for function in functions:
-        await function
-
-
-async def run_parallel(*functions: Awaitable[Any]) -> None:
-    await asyncio.gather(*functions)
-
-
 async def main() -> None:
-    # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
 
-    task1 = asyncio.create_task(service.register_device(hue_light))
-    task2 = asyncio.create_task(service.register_device(speaker))
-    task3 = asyncio.create_task(service.register_device(toilet))
+    device_task1 = asyncio.create_task(service.register_device(hue_light))
+    device_task2 = asyncio.create_task(service.register_device(speaker))
+    device_task3 = asyncio.create_task(service.register_device(toilet))
 
-    # hue_light_id = service.register_device(hue_light)
-    # speaker_id = service.register_device(speaker)
-    # toilet_id = service.register_device(toilet)
-
-    hue_light_id = await task1
-    speaker_id = await task2
-    toilet_id = await task3
-    # create a few programs
+    await asyncio.gather(device_task1, device_task2, device_task3)
 
     wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(device_task1.result(), MessageType.SWITCH_ON),
+        Message(device_task2.result(), MessageType.SWITCH_ON),
+        Message(
+            device_task3.result(),
+            MessageType.PLAY_SONG,
+            "Rick Astley - Never Gonna Give You Up"
+        ),
     ]
 
     sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
+        Message(device_task1.result(), MessageType.SWITCH_OFF),
+        Message(device_task2.result(), MessageType.SWITCH_OFF),
+        Message(device_task3.result(), MessageType.FLUSH),
+        Message(device_task3.result(), MessageType.CLEAN),
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await service.run_sequence(*wake_up_program)
+    await service.run_parallel(*sleep_program)
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
